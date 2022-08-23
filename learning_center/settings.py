64b06fd7d96c5c7715a41205d9472f7004672efd
Soroush Gholamzadeh <s.s.gholamzadeh@gmail.com>
2022-08-23 11:52:42 +0430

    bug fix 0.0.3

--- learning_center/settings.py
+++ learning_center/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x3kt5lhb)imd#rvdhk8pxp=nzuc3l9r5$dgz!c)@(z!n=-yjfz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['xtechno.ir', 'http://xtechno.ir', 'www.xtechno.ir', '*']
 
 
 # Application definition
